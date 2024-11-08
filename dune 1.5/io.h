7eb6970263e0a0e546b4740f8e43d3c309c45244
jungdongu <ch3030aa@gmail.com>
2024-11-08 12:49:46 +0900

    문자열 함수추가

--- dune 1.5/io.h
+++ dune 1.5/io.h
@@ -4,12 +4,13 @@
 #ifndef _IO_H_
 #define _IO_H_
 
+
 #include "common.h"
 
 KEY get_key(void);
 void printc(POSITION pos, char ch, int color , int bag);
 void gotoxy(POSITION pos);
 void set_color(int color, int bag);
-
+void printc2(POSITION pos, char ch[100], int color, int bag);
 #endif
 
