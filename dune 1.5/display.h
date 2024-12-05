9cce332a1180177e803a0eea7ac6a432376d8517
jungdongu <ch3030aa@gmail.com>
2024-12-06 03:54:43 +0900

    ( 6 ) 건물 건설 함수 추가 후 ( 6 ) 완료

--- dune 1.5/display.h
+++ dune 1.5/display.h
@@ -18,6 +18,7 @@
 
 
 
+
 // 지금은 자원, 맵, 커서만 표시
 // 앞으로 화면에 표시할 내용들 여기에 추가하기
 void display(
@@ -25,9 +26,17 @@ void display(
 	char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH],
 	CURSOR cursor
 );
-void sel_tp(void);
-void info_re(void);
-void h_cre(void);
-void sys_mes(void);
 
+void sel_tp(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH], RESOURCE resource);
+void info_re(void);
+void h_cre(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH]);
+void sys_mes(void);
+void command_re(void);
+void build_list(void);
+void build_P(void);
+void build_G(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH]);
+void build_D(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH]);
+void build_S(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH]);
+void build_B(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH]);
+void tower_cre(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH]);
 #endif
