5d7c74bd75dcb7861dcaf228baa86afff734021b
jungdongu <ch3030aa@gmail.com>
2024-12-10 07:00:55 +0900

    듄 최종체출

--- dune 1.5/io.h
+++ dune 1.5/io.h
@@ -12,5 +12,6 @@ void printc(POSITION pos, char ch, int color , int bag);
 void gotoxy(POSITION pos);
 void set_color(int color, int bag);
 void printc2(POSITION pos, char ch[100], int color, int bag);
+void printc3(POSITION pos, int ch, int color, int bag);
 #endif
 
