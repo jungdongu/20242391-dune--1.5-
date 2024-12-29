d8dfd151e2e27f19f2100fe7487fa1e755061eeb
jungdongu <ch3030aa@gmail.com>
2024-12-29 15:32:56 +0900

    코드 최종수정

--- dune 1.5/io.h
+++ dune 1.5/io.h
@@ -8,7 +8,7 @@
 #include "common.h"
 
 KEY get_key(void);
-void printc(POSITION pos, char ch, int color , int bag);
+void printc(POSITION pos, char ch, int color, int bag);
 void gotoxy(POSITION pos);
 void set_color(int color, int bag);
 void printc2(POSITION pos, char ch[100], int color, int bag);
